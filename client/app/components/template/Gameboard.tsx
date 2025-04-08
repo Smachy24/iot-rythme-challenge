6e4f2d598764623c09d037fd37e68d5e9cf56125
Val <pro.mvaldo@gmail.com>
2025-04-08 20:28:18 +0200

    feat: columns for square and detect input pressed according to square data

--- client/app/components/template/Gameboard.tsx
+++ client/app/components/template/Gameboard.tsx
@@ -2,7 +2,7 @@ import Screen from "../Screen";
 
 export default function Gameboard() {
     return (
-        <div className="flex flex-col items-center border-2 border-amber-200">
+        <div className="flex flex-col items-center">
             <h1 className="text-4xl text-white">Gameboard</h1>
             <Screen />
         </div>
